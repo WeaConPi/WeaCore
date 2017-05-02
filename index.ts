669c4ff341d107f5a8e581b69f840b5e75aec023
Vojtech Baránek <vojtabaranek@gmail.com>
2017-05-02 13:10:52 +0200

    warning and deprecation fix

--- index.ts
+++ index.ts
@@ -7,8 +7,8 @@ const app = express();
 const PORT = (process.env.PORT || 4000);
 
 app.get('/dummyprocesstest', (request, response) => {
-    const hourId = String(request.param('hourId'));
-    handleNewMeasurementHook(hourId)
+    const hourId = String(request.query.hourId);
+    handleNewMeasurementHook(hourId).catch(console.log)
     response.send("forecasted")
 });
 
@@ -17,19 +17,20 @@ app.get('/hello', (request, response) => {
 });
 
 app.get('/get-brain-calculation', (request, response) => {
-    const day = Number(request.param('day'));
-    const month = Number(request.param('month'));
-    const hour = Number(request.param('hour'));
-    const temp = Number(request.param('temp'));
-    const houseTemp = Number(request.param('houseTemp'));
-    const result = getCalculationPure(day, month, hour, temp, houseTemp);
+    const day = Number(request.query.day);
+    const month = Number(request.query.month);
+    const hour = Number(request.query.hour);
+    const temp = Number(request.query.temp);
+    const houseTemp = Number(request.query.houseTemp);
+    console.log('Requesting brain calculation');
+    const result = getCalculationPure(month, day, hour, temp, houseTemp);
     response.send(result)
 });
 
 app.get('/measurement-hook', (request, response) => {
 
-    const hourId = String(request.param('hourId'));
-    handleNewMeasurementHook(hourId)
+    const hourId = String(request.query.hourId);
+    handleNewMeasurementHook(hourId).catch(console.log);
     response.send("Hello")
 });
 
--- service/PredictionHandler.ts
+++ service/PredictionHandler.ts
@@ -1,42 +1,53 @@
-import { getForecastForLonLat } from "./WeatherForecast";
-import { mapResponseForecastToForecastWeather } from "../model/ForecastWeather";
-import { persistForecastByHourID } from "../repository/ForecastRepository";
-import { Hour, IHour } from "../model/Hour";
-import * as rp from "request-promise";
-import { weaNenHost } from "../network";
-import { persistPredictionByHourID } from "../repository/PredictionRepository";
-import { mapResponseToPrediction } from "../model/Prediction";
+import { getForecastForLonLat } from './WeatherForecast';
+import { mapResponseForecastToForecastWeather } from '../model/ForecastWeather';
+import { persistForecastByHourID } from '../repository/ForecastRepository';
+import { Hour, IHour } from '../model/Hour';
+import * as rp from 'request-promise';
+import { weaNenHost } from '../network';
+import { persistPredictionByHourID } from '../repository/PredictionRepository';
+import { mapResponseToPrediction } from '../model/Prediction';
 
 /**
  * Created by Farmas on 24.04.2017.
  */
 
 export const handleNewMeasurementHook = async (hourId: string) => {
-    const forecast = await getForecastForLonLat();
-    const forecastWeather = mapResponseForecastToForecastWeather(forecast);
-    let insertedHour = await persistForecastByHourID(hourId, forecastWeather);
-    const hour = new Hour(insertedHour)
-    getCalculatedPrediction(hour)
-
-}
+  const forecast = await getForecastForLonLat();
+  const forecastWeather = mapResponseForecastToForecastWeather(forecast);
+  let insertedHour = await persistForecastByHourID(hourId, forecastWeather);
+  const hour = new Hour(insertedHour);
+  getCalculatedPrediction(hour).catch(console.log);
+};
 const getCalculatedPrediction = async (hour: IHour) => {
-    const today = new Date();
-    const tempSensors = hour.sensors.filter(sensor => sensor.type === 'Temperature');
-    //TODO refactor to use getCalculationPure method
-    if (tempSensors && tempSensors[0]) {
-        const requestURL = `${weaNenHost}/calculate?day=${today.getDay()}&month=${today.getMonth()}&hour=${hour.number}&temp=${hour.forecast.temperature}&houseTemp=${tempSensors[0].value}`
-        console.log('Requesting calculate')
-        console.log(requestURL)
-        const prediction = await rp(requestURL)
-        const calcPrediction = mapResponseToPrediction(prediction)
-        persistPredictionByHourID(hour.id, calcPrediction)
-    }
-}
+  const today = new Date();
+  const tempSensors = hour.sensors.filter(
+    sensor => sensor.type === 'Temperature'
+  );
+  //TODO refactor to use getCalculationPure method
+  if (tempSensors && tempSensors[0]) {
+    const requestURL = `${weaNenHost}/calculate?day=${today.getDay()}&month=${today.getMonth()}&hour=${hour.number}&temp=${hour.forecast.temperature}&houseTemp=${tempSensors[0].value}`;
+    console.log('Requesting calculate');
+    console.log(requestURL);
+    const prediction = await rp(requestURL);
+    const calcPrediction = mapResponseToPrediction(prediction);
+    persistPredictionByHourID(hour.id, calcPrediction).catch(console.log);
+  }
+};
 
-export const getCalculationPure = async (month: number, day: number, hour: number, temperature: number, houseTemp: number) => {
-    const requestURL = `${weaNenHost}/calculate?day=${day}&month=${month}&hour=${hour}&temp=${temperature}&houseTemp=${houseTemp}`
-    console.log('Requesting calculate')
-    console.log(requestURL)
-    const prediction = await rp(requestURL)
-    return prediction;
-}
+export const getCalculationPure = async (
+  month: number,
+  day: number,
+  hour: number,
+  temperature: number,
+  houseTemp: number
+) => {
+  const requestURL = `${weaNenHost}/calculate?day=${day}&month=${month}&hour=${hour}&temp=${temperature}&houseTemp=${houseTemp}`;
+  console.log('Requesting calculate');
+  console.log(requestURL);
+  try {
+    return await rp(requestURL);
+  } catch (e) {
+    console.log('Error requesting pure calculation');
+    return Error('Ops cannot use my brain');
+  }
+};
