2527bd4f7a2ccce76a46839bab0695f53616c396
Vojtech Baránek <vojtabaranek@gmail.com>
2017-05-02 13:29:49 +0200

    cache control

--- index.ts
+++ index.ts
@@ -1,45 +1,50 @@
 /**
  * Created by Farmas on 20.04.2017.
  */
-import * as express from "express";
-import { handleNewMeasurementHook, getCalculationPure } from "./service/PredictionHandler";
+import * as express from 'express';
+import {
+  handleNewMeasurementHook,
+  getCalculationPure,
+} from './service/PredictionHandler';
 const app = express();
-const PORT = (process.env.PORT || 4000);
+const PORT = process.env.PORT || 4000;
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, cache-control, Content-Type, Accept'
+  );
+  next();
 });
 
 app.get('/dummyprocesstest', (request, response) => {
-    const hourId = String(request.query.hourId);
-    handleNewMeasurementHook(hourId).catch(console.log)
-    response.send("forecasted")
+  const hourId = String(request.query.hourId);
+  handleNewMeasurementHook(hourId).catch(console.log);
+  response.send('forecasted');
 });
 
 app.get('/hello', (request, response) => {
-    response.send("Hello")
+  response.send('Hello');
 });
 
 app.get('/get-brain-calculation', async (request, response) => {
-    const day = Number(request.query.day);
-    const month = Number(request.query.month);
-    const hour = Number(request.query.hour);
-    const temp = Number(request.query.temp);
-    const houseTemp = Number(request.query.houseTemp);
-    console.log('Requesting brain calculation');
-    const result = await getCalculationPure(month, day, hour, temp, houseTemp);
-    response.send(result)
+  const day = Number(request.query.day);
+  const month = Number(request.query.month);
+  const hour = Number(request.query.hour);
+  const temp = Number(request.query.temp);
+  const houseTemp = Number(request.query.houseTemp);
+  console.log('Requesting brain calculation');
+  const result = await getCalculationPure(month, day, hour, temp, houseTemp);
+  response.send(result);
 });
 
 app.get('/measurement-hook', (request, response) => {
-
-    const hourId = String(request.query.hourId);
-    handleNewMeasurementHook(hourId).catch(console.log);
-    response.send("Hello")
+  const hourId = String(request.query.hourId);
+  handleNewMeasurementHook(hourId).catch(console.log);
+  response.send('Hello');
 });
 
 app.listen(PORT, () => {
-    console.log(`Start is up and running on localhost:${PORT}`)
+  console.log(`Start is up and running on localhost:${PORT}`);
 });
